570e91eaaca18150100bdacd943bc9b2651743eb
summer51202 <summer51202@gmail.com>
2024-10-29 18:30:59 +0800

    [UPDATE] update new embedding model

--- eval.py
+++ eval.py
@@ -4,56 +4,77 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some paths and files.')
     parser.add_argument('--groundtruth_path', type=str, default="./dataset/preliminary/ground_truths_example.json", help="path to ground truth json")  # 問題文件的路徑
-    parser.add_argument('--submission_path', type=str, default="./dataset/preliminary/sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2/answer_Chroma_50_25.json", help="path to submission json")  # 參考資料的路徑
+    parser.add_argument('--submission_path', type=str, default="./dataset/preliminary/BAAI/bge-small-zh-v1.5/answer_Chroma_100_50.json", help="path to submission json")  # 參考資料的路徑
+import json
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some paths and files.')
+    parser.add_argument('--question_path', type=str, default="./dataset/preliminary/questions_example_revision.json", help="path to question json")  # 問題文件的路徑
+    parser.add_argument('--groundtruth_path', type=str, default="./dataset/preliminary/ground_truths_example_revision.json", help="path to ground truth json")  # 問題文件的路徑
+    parser.add_argument('--submission_path', type=str, default="./dataset/preliminary/BAAI/bge-small-zh-v1.5/answer_Chroma_100_50.json", help="path to submission json")  # 參考資料的路徑
 
     args = parser.parse_args()
 
-    with open(args.groundtruth_path, 'r') as f:
+    with open(args.question_path, 'r', encoding='utf-8') as f:
+        question = json.load(f)  # 讀取問題檔案
+
+    with open(args.groundtruth_path, 'r', encoding='utf-8') as f:
         ground_truth = json.load(f)  # 讀取問題檔案
-        print("%s is loaded."%args.groundtruth_path)
     
-    with open(args.submission_path, 'r') as f:
+    with open(args.submission_path, 'r', encoding='utf-8') as f:
         submission = json.load(f)  # 讀取問題檔案
-        print("%s is loaded."%args.submission_path)
 
-
-    ground_truth = ground_truth["ground_truths"]
-    submission = submission["answers"]
-    # ground_truth = ground_truth["ground_truths"][:50]
-    # submission = submission["answers"][:50]
-    # ground_truth = ground_truth["ground_truths"][50:100]
-    # submission = submission["answers"][50:100]
-    # ground_truth = ground_truth["ground_truths"][100:]
-    # submission = submission["answers"][100:]
+    q = question["questions"]
     # 計算分數
-
     precision = 0
-    precision50 = 0
-    precision100 = 0
-    precision150 = 0
-    wrong_qids = []
-    for sub, gt in zip(submission, ground_truth):
+    n_wrong = 0
+    precision_insurance = 0
+    precision_finance = 0
+    precision_faq = 0
+    insurance_count = 0
+    finance_count = 0
+    faq_count = 0
+
+    n_questions = len(ground_truth["ground_truths"])
+    wrong_qid = []
+    print("錯誤題目：")
+    print("=" * 100)
+    for sub, gt in zip(submission["answers"], ground_truth["ground_truths"]):
         if sub["qid"] != gt["qid"]:
-            print("qid not match")
-            break
+            assert ValueError(f"qid not match: {sub['qid']} != {gt['qid']}")
+
+        if 1 <= gt["qid"] <= 50:
+            if sub["retrieve"] == gt["retrieve"]:
+                precision_insurance += 1
+            insurance_count += 1
+
+        elif 51 <= gt["qid"] <= 100:
+            if sub["retrieve"] == gt["retrieve"]:
+                precision_finance += 1
+            finance_count += 1
+
+        elif 101 <= gt["qid"] <= 150:
+            if sub["retrieve"] == gt["retrieve"]:
+                precision_faq += 1
+            faq_count += 1
+
+
         if sub["retrieve"] == gt["retrieve"]:
             precision += 1
         else:
-            wrong_qids.append(str(sub["qid"]))
-        if sub["qid"] == 50:
-            precision50 = precision / 50
-            precision = 0
-        if sub["qid"] == 100: 
-            precision100 = precision / 50
-            precision = 0
-        if sub["qid"] == 150: 
-            precision150 = precision / 50
-            precision = 0
+            n_wrong += 1
+            wrong_qid.append(gt["qid"])
+            print("qid {}: {} ans: {}, gt: {}".format(gt["qid"], q[sub["qid"]-1]["query"], sub["retrieve"], gt["retrieve"]))
 
-    print("Wrong qid: %s"% ",".join(wrong_qids))
-    print("Insurance Precision: {:.2f} %".format(precision50 * 100))
-    print("Finance Precision: {:.2f} %".format(precision100 * 100))
-    print("FAQ Precision: {:.2f} %".format(precision150 * 100))
+        if gt["qid"] == 50 or gt["qid"] == 100 or gt["qid"] == 150:
+            print("=" * 100)
 
-    avg_precision = (precision50 + precision100 + precision150)/3
-    print("Avg Precision: {:.2f} %".format(avg_precision * 100))
+    precision /= n_questions
+    print(f"Wrong id: " + ", ".join([str(item) for item in wrong_qid]))
+    print("Total questions: {}, Wrong answers: {}".format(n_questions, n_wrong))
+    print(f"Insurance precision: {precision_insurance/50 * 100:.2f} %; ({precision_insurance}/50)")
+    print(f"Finance precision:   {precision_finance/50 * 100:.2f} %; ({precision_finance}/50)")
+    print(f"FAQ precision:       {precision_faq/50 * 100:.2f} %; ({precision_faq}/50)")
+    print(f"Total Precision:     {precision * 100:.2f} %: ({n_questions - n_wrong}/{n_questions})")
---
+++ src/preprocess/text_process.py
@@ -0,0 +1,117 @@
+import re
+import cn2an
+import string
+
+def season_convert(text):
+    pattern = r"第[一二三四1234]季"
+    matched = re.search(pattern, text)
+    if matched:
+        season_str = matched.group(0)
+        season = season_str.replace("第", "").replace("季", "")
+        season = cn2an.cn2an(season, "smart")
+        
+        season_months = ""
+        if season == 1:
+            season_months = "1月2月3月"
+        elif season == 2:
+            season_months = "4月5月6月"
+        elif season == 3:
+            season_months = "7月8月9月"
+        elif season == 4:
+            season_months = "10月11月12月"
+
+        replace_date = "{}/{}".format(season_str, season_months)
+        text = re.sub(pattern, replace_date, text)
+    return text
+
+def age_convert(text):
+    pattern = r"[一二三四五六七八九零十百○]+(歲|足歲)"
+    matched = re.search(pattern, text)
+    if matched:
+        age_str = matched.group(0)
+        age = age_str.replace("足歲", "").replace("歲", "").replace("○", "零")
+        age = str(cn2an.cn2an(age, "smart"))
+        
+        replace_date = "{}/{}".format(age, age_str)
+        text = re.sub(pattern, replace_date, text)
+    return text
+
+def ROCC_convert(text):    
+    pattern = r"(?<!\d)\d{2,3}(?!\d)年"
+    matched = re.search(pattern, text)
+    if matched:
+        date_str = matched.group(0)
+        year = date_str.replace("年", "")
+        year = int(year)+1911
+        replace_date = "{}/{}年".format(date_str, year)
+        text = re.sub(pattern, replace_date, text)
+
+    pattern = r"民國[一二三四五六七八九零十百○]+年[一二三四五六七八九零十百○]+月[一二三四五六七八九零十百○]+日"
+    matched = re.search(pattern, text)
+    if matched:
+        date_str = matched.group(0)
+        
+        m_year = re.search("民國(.*)年", date_str)
+        year = m_year.group(0).replace("民國", "").replace("年", "").replace("○", "零")
+        year = cn2an.cn2an(year, "smart")
+        year = int(year)+1911
+
+        m_month = re.search("年(.*)月", date_str)
+        month = m_month.group(0).replace("年", "").replace("月", "")
+        month = cn2an.cn2an(month, "smart")
+
+        m_day = re.search("月(.*)日", date_str)
+        day = m_day.group(0).replace("月", "").replace("日", "")
+        day = cn2an.cn2an(day, "smart")
+
+        replace_date = "{}/{}年{}月{}日".format(date_str, year, month, day)
+        text = re.sub(pattern, replace_date, text)
+    return text
+
+def remove_punctuation(text):
+    punc = "！？｡。＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､、〃》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏.\n\/ /"
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = re.sub("[{}]+".format(punc), "", text)
+    return text
+
+def explain_proper_noun(text):
+    explain_dict = {
+        "realtek": "瑞昱",
+        "瑞昱": "realtek",
+
+    }
+    for noun in explain_dict.keys():
+        if re.search(noun, text, re.IGNORECASE):
+            text = re.sub(noun, noun+explain_dict[noun], text, flags=re.IGNORECASE)
+        
+    return text
+
+def kelvin_preprocess(text):
+    # [Kelvin] 清除特殊符號
+    text = remove_punctuation(text)
+    # [Kelvin] 民國年轉換
+    text = ROCC_convert(text)
+    # [Kelvin] 季度轉換
+    #text = season_convert(text)
+    # [Kelvin] 歲數轉換
+    #text = age_convert(text)
+    return text
+
+def edward_preprocess(text):
+    # [Kelvin] 清除特殊符號
+    text = remove_punctuation(text)
+    # text = explain_proper_noun(text)
+    return text
+
+
+########################################################################################################################
+def read_faq_data(faq_items: list) -> str:
+    text = ""
+    for item in faq_items:
+        if 'question' in item:
+            text += item['question'] + " "
+        if 'answers' in item:
+            for temp_answer in item['answers']:
+                text += temp_answer + " "
+    text = ''.join(text.splitlines())
+    return text
